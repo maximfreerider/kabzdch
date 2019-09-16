46a113c8aeb36351f7884eb347a474af65d806bc
maximfreerider <ivanovma41@gmail.com>
2019-09-16 11:30:41 +0300

    Fixed bug with following

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,6 +2,52 @@ import React from 'react';
 import s from './Users.module.css';
 
 const Users = (props) => {
+debugger
+    // что бы избежать зациклевания компоненты
+    if (props.users.length === 0) {
+        props.setUsers([
+            {
+                id: 1,
+                photoUrl: 'https://vokrug.tv/pic/news/d/3/d/5/d3d530210cd546b48e3b7ca34559adb5.jpg',
+                fullName: 'Maks',
+                followed: true,
+                status: 'I am a boss',
+                location: {city: 'Minsk', country: 'Belarus'}
+            },
+            {
+                id: 2,
+                photoUrl: 'https://vokrug.tv/pic/news/d/3/d/5/d3d530210cd546b48e3b7ca34559adb5.jpg',
+                fullName: 'Sasha',
+                followed: true,
+                status: 'Iss am a boss',
+                location: {city: 'Moscow', country: 'Russia'}
+            },
+            {
+                id: 3,
+                photoUrl: 'https://vokrug.tv/pic/news/d/3/d/5/d3d530210cd546b48e3b7ca34559adb5.jpg',
+                fullName: 'Alex',
+                followed: false,
+                status: 'I amsfdwef a boss',
+                location: {city: 'Lvov', country: 'Ukraine'}
+            },
+            {
+                id: 4,
+                photoUrl: 'https://vokrug.tv/pic/news/d/3/d/5/d3d530210cd546b48e3b7ca34559adb5.jpg',
+                fullName: 'Alex',
+                followed: false,
+                status: 'I amsfdwef a boss',
+                location: {city: 'Lvov', country: 'Ukraine'}
+            },
+            {
+                id: 5,
+                photoUrl: 'https://vokrug.tv/pic/news/d/3/d/5/d3d530210cd546b48e3b7ca34559adb5.jpg',
+                fullName: 'John',
+                followed: true,
+                status: 'I am a bowewess',
+                location: {city: 'Berlin', country: 'Germany'}
+            }
+        ]);
+    }
     return (
         <div>
             {
@@ -10,7 +56,7 @@ const Users = (props) => {
                         <div key={user.id}>
                             <span>
                                 <div>
-                                    <img className={s.userPhoto} src={user.photoUrl}/>
+                                    <img alt="Userphoto" className={s.userPhoto} src={user.photoUrl}/>
                                 </div>
                                 <div>
                                     {
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,13 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SETUSERS = 'SETUSERS';
 
 let initialState = {
-    users: [
-        {id: 1, photoUrl: 'https://vokrug.tv/pic/news/d/3/d/5/d3d530210cd546b48e3b7ca34559adb5.jpg', fullName: 'Maks', followed: true, status: 'I am a boss', location: {city: 'Minsk', country: 'Belarus'}},
-        {id: 1, photoUrl: 'https://vokrug.tv/pic/news/d/3/d/5/d3d530210cd546b48e3b7ca34559adb5.jpg', fullName: 'Sasha', followed: true, status: 'Iss am a boss', location: {city: 'Moscow', country: 'Russia'}},
-        {id: 1, photoUrl: 'https://vokrug.tv/pic/news/d/3/d/5/d3d530210cd546b48e3b7ca34559adb5.jpg', fullName: 'Alex', followed: false, status: 'I amsfdwef a boss', location: {city: 'Lvov', country: 'Ukraine'}},
-        {id: 1, photoUrl: 'https://vokrug.tv/pic/news/d/3/d/5/d3d530210cd546b48e3b7ca34559adb5.jpg', fullName: 'Alex', followed: false, status: 'I amsfdwef a boss', location: {city: 'Lvov', country: 'Ukraine'}},
-        {id: 1, photoUrl: 'https://vokrug.tv/pic/news/d/3/d/5/d3d530210cd546b48e3b7ca34559adb5.jpg', fullName: 'John', followed: true, status: 'I am a bowewess', location: {city: 'Berlin', country: 'Germany'}}
-    ]
+    users: []
 };
 
 const usersReducer = (state=initialState, action) => {
