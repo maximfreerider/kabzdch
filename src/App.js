d41f87ffee8ba2dea67c3e49c3ec6148cdfbb239
maximfreerider <ivanovma41@gmail.com>
2019-09-22 13:44:27 +0300

    Created auth-reducer, added login navlink to Header , now user can authenticate in our social network:)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import {Route, BrowserRouter} from 'react-router-dom';
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 const App = (props) => {
@@ -13,7 +13,7 @@ const App = (props) => {
         <div>
             <BrowserRouter>
                 <div className='app-wrapper'>
-                    <Header/>
+                    <HeaderContainer/>
                     <Navbar/>
                     <div className='app-wrapper-content'>
                         {/*<Route path="/dialogs" component={Dialogs}/>*/}
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import sidebarReducer from "./sidebar-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 let redusers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
 let store = createStore(redusers);
