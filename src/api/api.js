b321f9492a467e98cc92880a45c883cb95fc2392
maximfreerider <ivanovma41@gmail.com>
2019-09-22 17:42:22 +0300

    Created a DAL level in project

---
+++ src/api/api.js
@@ -0,0 +1,8 @@
+import * as axios from "axios";
+
+export  const getUsers = (currentPage=1, pageSize=10) => {
+    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {withCredentials: true})
+        .then(response => {
+            return response.data;
+        });
+};
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -6,30 +6,29 @@ import {
     unfollow
 } from "../../redux/users-reducer";
 import React from "react";
-import * as axios from "axios";
 import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
+import {getUsers} from "../../api/api";
 
 class UsersContainerComponent extends React.Component {
 
     componentDidMount(){
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {withCredentials:true})
-            .then(responce => {
+        getUsers(this.props.currentPage, this.props.pageSize).then(data => {
+                debugger;
                 this.props.toggleIsFetching(false);
-                this.props.setUsers(responce.data.items);
-                console.log(responce);
-                this.props.setTotalUsersCount(responce.data.totalCount);
+                this.props.setUsers(data.items);
+                this.props.setTotalUsersCount(data.totalCount);
             });
     };
 
     onPageChanged = (numberOfPage) => {
         this.props.setCurrentPage(numberOfPage);
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${numberOfPage}&count=${this.props.pageSize}`, {withCredentials: true})
-            .then(responce => {
+        getUsers(numberOfPage, this.props.pageSize).then(data => {
+            debugger;
                 this.props.toggleIsFetching(false);
-                this.props.setUsers(responce.data.items);
+                this.props.setUsers(data.items);
             });
     };
 
